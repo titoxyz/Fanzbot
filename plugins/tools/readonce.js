36e11a163a9c7489fec6086ae09a70e7ba626f17
AgusXzz <davidmersey707@gmail.com>
2025-09-13 14:43:06 +0000

    # Jawir Perubahan

--- plugins/tools/readonce.js
+++ plugins/tools/readonce.js
@@ -3,7 +3,7 @@ export default {
   category: "tools",
   command: ["rvo"],
   run: async (conn, m) => {
-    if (!m.quoted?.msg?.viewOnce) return m.reply("Not a one-time view media!!");
+    if (!m.quoted?.msg?.viewOnce) throw "Not a one-time view media!!";
     m.quoted.msg.viewOnce = false;
     m.reply({ forward: m.quoted, force: true });
   }
