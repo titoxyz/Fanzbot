72f0e7bf970e82d2450e8260f24539c10b3f099a
Agus <davidmersey707@gmail.com>
2025-09-18 12:38:21 +0800

    Biar keliatan bnyak update

--- plugins/sticker/brat.js
+++ plugins/sticker/brat.js
@@ -6,7 +6,7 @@ export default {
         if (!m.text) return m.reply('Masukkan teks yang valid!')
 
         try {
-            let res = Api.request('brat', "/api/brat" { text: m.text.trim() })
+            let res = Api.request('brat', "/api/brat", { text: m.text.trim() })
             conn.sendSticker(m.chat, res.URL, m)
         } catch (err) {
             console.error('Error:', err)
