47222d81479ab16c0e3ef2082dde13cd15af70c8
Agus <davidmersey707@gmail.com>
2025-09-18 12:34:58 +0800

    Ngantuk

--- plugins/sticker/brat.js
+++ plugins/sticker/brat.js
@@ -1,17 +1,12 @@
 export default {
     name: "brat",
     category: "sticker",
-    command: ["brat", "bratvid", "bratvideo"],
+    command: ["brat"],
     run: async (conn, m, { Api }) => {
         if (!m.text) return m.reply('Masukkan teks yang valid!')
 
         try {
-            let end = '/api/brat?text='
-            if (/vid|video/i.test(m.command)) {
-                end = '/api/bratvid?text='
-            }
-
-            let res = Api.request('brat', end + encodeURIComponent(m.text.trim()))
+            let res = Api.request('brat', "/api/brat" { text: m.text.trim() })
             conn.sendSticker(m.chat, res.URL, m)
         } catch (err) {
             console.error('Error:', err)
