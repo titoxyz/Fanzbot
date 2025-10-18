774d307ad678829f0341025742358ec8a922961c
AgusXzz <davidmersey707@gmail.com>
2025-10-18 08:15:16 +0700

    Update

--- plugins/ai-chat/kimi.js
+++ plugins/ai-chat/kimi.js
@@ -1,20 +1,20 @@
 import deepseek from '#scrape/deepinfra.js';
 
 export default {
-    name: "deepseek",
+    name: "kimi",
     category: "ai-chat",
-    command: ["deepseek", "dipsek"],
+    command: ["kimi"],
     run: async (conn, m) => {
         const input = m.isQuoted ? m.quoted.text : m.text;
         if (!input) return m.reply(`Masukkan pertanyaan atau perintah!\n\nContoh:\n${m.cmd} apa itu AI`);
 
-        if (!conn.deepseek) conn.deepseek = {};
-        if (!conn.deepseek[m.sender]) conn.deepseek[m.sender] = [];
-        conn.deepseek[m.sender].push({ role: 'user', content: input });
+        if (!conn.kimi) conn.kimi = {};
+        if (!conn.kimi[m.sender]) conn.kimi[m.sender] = [];
+        conn.kimi[m.sender].push({ role: 'user', content: input });
 
         try {
-            const res = await deepseek('deepseek-ai/DeepSeek-V3', conn.deepseek[m.sender]);
-            conn.deepseek[m.sender].push({ role: 'assistant', content: res });
+            const res = await deepseek('moonshotai/Kimi-K2-Instruct-0905', conn.kimi[m.sender]);
+            conn.kimi[m.sender].push({ role: 'assistant', content: res });
             m.reply(res)
         } catch (err) {
             m.reply('Terjadi Kesalahan')
