41585a223c497e8b3e8336f717da716f7c0882db
Vrypt <keriptozzzzz@gmail.com>
2025-10-29 07:24:01 +0700

    clean index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import makeWASocket, {
 import { Boom } from '@hapi/boom'
 import fs from 'fs'
 import pino from 'pino'
-
 import serialize, { Client } from '#lib/serialize.js'
 import log from '#lib/logger.js'
 import PluginsLoad from '#lib/loadPlugins.js'
@@ -39,7 +38,6 @@ async function startWA() {
   })
 
   await Client(conn)
-
   conn.chats ??= {}
 
   if (!conn.authState.creds.registered) {
@@ -55,7 +53,6 @@ async function startWA() {
 
   conn.ev.on('connection.update', async ({ connection, lastDisconnect }) => {
     if (connection) log.info(`Connection Status: ${connection}`)
-
     if (connection === 'close') {
       const statusCode = new Boom(lastDisconnect?.error)?.output?.statusCode
       switch (statusCode) {
@@ -90,56 +87,51 @@ async function startWA() {
         default:
           log.error(`Unhandled connection issue. Code: ${statusCode}`)
           await startWA()
-          //return process.exit(1)
       }
     }
-
     if (connection === 'open') {
       log.success('Bot connected successfully.')
-      conn.chats = await conn.groupFetchAllParticipating();
+      conn.chats = await conn.groupFetchAllParticipating()
     }
   })
 
   conn.ev.on('creds.update', saveCreds)
 
-  conn.ev.on("groups.update", (updates) => {
-        for (const update of updates) {
-            const id = update.id
-            if (conn.chats[id]) {
-                conn.chats[id] = {
-                    ...(conn.chats[id] || {}),
-                    ...(update || {})
-                }
-            }
+  conn.ev.on('groups.update', (updates) => {
+    for (const update of updates) {
+      const id = update.id
+      if (conn.chats[id]) {
+        conn.chats[id] = {
+          ...(conn.chats[id] || {}),
+          ...(update || {})
         }
-    })
+      }
+    }
+  })
 
-    conn.ev.on('group-participants.update', ({ id, participants, action }) => {
-        const metadata = conn.chats[id]
-        if (metadata) {
-            switch (action) {
-                case 'add':
-                case "revoked_membership_requests":
-                    participants.map(p => metadata.participants.push(p))
-                    break
-                case 'demote':
-                case 'promote':
-                    for (const p of participants) {
-                        const target = metadata.participants.find(x => x.id === p.id);
-                        if (target) {
-                            target.admin = (action === 'promote') ? 'admin' : null;
-                        }
-                    }
-                    break
-                case 'remove':
-                    metadata = {
-                        ...metadata,
-                        participants: metadata.participants.filter(p => !participants.includes(p.id))
-                    }
-                    break
+  conn.ev.on('group-participants.update', ({ id, participants, action }) => {
+    const metadata = conn.chats[id]
+    if (metadata) {
+      switch (action) {
+        case 'add':
+        case 'revoked_membership_requests':
+          participants.map(p => metadata.participants.push(p))
+          break
+        case 'demote':
+        case 'promote':
+          for (const p of participants) {
+            const target = metadata.participants.find(x => x.id === p.id)
+            if (target) {
+              target.admin = action === 'promote' ? 'admin' : null
             }
-        }
-    })
+          }
+          break
+        case 'remove':
+          metadata.participants = metadata.participants.filter(p => !participants.includes(p.id))
+          break
+      }
+    }
+  })
 
   conn.ev.on('messages.upsert', async ({ messages }) => {
     if (!messages[0]) return
@@ -149,38 +141,34 @@ async function startWA() {
     conn.messages.get(m.chat).push(m)
 
     if (m.key && !m.fromMe && m.key.remoteJid === 'status@broadcast') {
-        if (!readsw.active) return
-        if (m.type === 'protocolMessage' && m.message.protocolMessage.type === 0) return;
-
-        try {
-            await conn.readMessages([m.key]);
-            if (!readsw.react) {
-                const emojis = readsw.emoji;
-                const emoji = emojis[Math.floor(Math.random() * emojis.length)] || '💚';
-                await delay(10000)
-                await conn.sendMessage('status@broadcast', {
-                    react: {
-                        text: emoji,
-                        key: m.key
-                    }
-                }, {
-                    statusJidList: [m.key.participant]
-                });
-            }
-            console.log(`Dibaca Story dari ${m.key.participant.split('@')[0]}`);
-        } catch (err) {
-            console.error(err);
+      if (!readsw.active) return
+      if (m.type === 'protocolMessage' && m.message.protocolMessage.type === 0) return
+      try {
+        await conn.readMessages([m.key])
+        if (!readsw.react) {
+          const emojis = readsw.emoji
+          const emoji = emojis[Math.floor(Math.random() * emojis.length)] || '💚'
+          await delay(10000)
+          await conn.sendMessage(
+            'status@broadcast',
+            { react: { text: emoji, key: m.key } },
+            { statusJidList: [m.key.participant] }
+          )
         }
+        console.log(`Dibaca Story dari ${m.key.participant.split('@')[0]}`)
+      } catch (err) {
+        console.error(err)
+      }
     }
 
     if (m.chat.endsWith('@broadcast') || m.chat.endsWith('@newsletter')) return
     if (m.message && !m.isBot) {
-    if (m.type == 'protocolMessage') return
-        await (await import(`./lib/print.js?v=${Date.now()}`)).default(conn, m)
+      if (m.type == 'protocolMessage') return
+      await (await import(`./lib/print.js?v=${Date.now()}`)).default(conn, m)
     }
 
     await (await import(`./handler.js?v=${Date.now()}`)).default(conn, m)
-})
+  })
 }
 
 startWA()
